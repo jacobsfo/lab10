4b47e0b0b520e1c7645a0dca3923186baf07ba11
Jacob Alamparambil <jacobsfo@gmail.com>
2020-05-18 15:54:23 -0700

    first

---
+++ turnin/jalam004_lab10_part1.c
@@ -0,0 +1,117 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work
+*/
+#include "timer.h"
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+unsigned char T,L;
+enum BL_States { BL_SMStart, LedOff, LedOn } BL_State;
+void TickFct_BlinkLed() 
+{
+	switch(BL_State)
+	{
+	case BL_SMStart: 
+	BL_State = LedOff;
+	break;
+	case LedOff: 
+	BL_State = LedOn;
+	break;
+	case LedOn: 
+	BL_State = LedOff;
+	break;
+	}
+	switch(BL_State)
+	{
+	case BL_SMStart: 
+	break;
+	case LedOff: 
+	L = 0;
+	break;
+	case LedOn: 
+	L = 1;
+	break;
+	}
+
+}	
+enum States { Start, Out} state;
+void Combine(){
+//unsigned char tmpB = 0x00;
+	switch(state)
+	{case Start: 
+	state = Out;
+	break;
+	case Out:
+	break;
+	}
+	switch(state)
+	{case Start: 
+	//state = Out;
+	break;
+	case Out:
+	PORTB = T | L;
+	}
+
+
+
+}
+enum TL_States { TL_SMStart, T0, T1, T2 } TL_State; 
+void TickFct_ThreeLeds() {
+   //... // Standard switch statements for SM
+	switch(TL_State)
+	{
+	case TL_SMStart: 
+	TL_State = T0;
+	break;
+	case T0: 
+	TL_State = T1;
+	break;
+	case T1: 
+	TL_State = T2;
+	break;
+	case T2:
+	TL_State = T0;
+	break;
+	}
+	switch(TL_State)
+	{
+	case TL_SMStart: 
+	break;
+	case T0: 
+	T = 0x01;
+	break;
+	case T1: 
+	T = 0x02;
+	break;
+	case T2:
+	T = 0x04;
+	break;
+
+	}
+
+}
+
+int main(void) {
+DDRB = 0xFF; PORTB = 0x00;
+//   PORTB = 0; // Init outputs
+   TimerSet(1000);
+   TimerOn(); 
+   BL_State = BL_SMStart;
+   TL_State = TL_SMStart;
+   state = Start; 
+   while (1) {          
+      TickFct_BlinkLed();    // Tick the BlinkLed synchSM
+      TickFct_ThreeLeds();
+      Combine();
+      //PORTB = tmpB;   // Tick the ThreeLeds synchSM
+      while (!TimerFlag){}   // Wait for timer period
+      TimerFlag = 0;         // Lower flag raised by timer
+   }
+}
